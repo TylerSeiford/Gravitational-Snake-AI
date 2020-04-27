6d45e90c63aa028a07541fe44110fad5eec34509
TylerSeiford <50031864+TylerSeiford@users.noreply.github.com>
2020-04-26 20:10:57 -0500

    Rearrange config.h

--- enc_temp_folder/338c7d9966f229e963e2a322781ffa64/config.h
+++ enc_temp_folder/338c7d9966f229e963e2a322781ffa64/config.h
@@ -7,8 +7,6 @@ constexpr double FPS = 100;
 constexpr bool VSYNC = true;
 constexpr bool FULLSCREEN = false;
 
-// GAMEPLAY SETTINGS
 constexpr bool AUDIO_ENABLED = false;
 constexpr bool ROCKS_ENABLED = false;
 constexpr bool WALLS_ENABLED = false;
-constexpr bool VSYNC = true;
